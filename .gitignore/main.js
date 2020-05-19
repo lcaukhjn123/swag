7fc5e880d229ee316594a8221b432ea1d14a6705
lcaukhjn123 <64785047+lcaukhjn123@users.noreply.github.com>
2020-05-19 18:17:59 +0200

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-bot.login(NzEyMjczNDMxNTk2NDk4OTQ0.XsPs9A.Lc3bcz5958jABk6z4qJPGPX_lvQ);
+bot.login();
 
 bot.on("ready", async () => {
     console.log("(TutoDev) : En ligne");
