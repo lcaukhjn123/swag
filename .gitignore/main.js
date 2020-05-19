906b1b4fa622d69ac820937822706aa7063ce973
lcaukhjn123 <64785047+lcaukhjn123@users.noreply.github.com>
2020-05-19 18:16:42 +0200

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+
+bot.login(NzEyMjczNDMxNTk2NDk4OTQ0.XsPs9A.Lc3bcz5958jABk6z4qJPGPX_lvQ);
+
+bot.on("ready", async () => {
+    console.log("(TutoDev) : En ligne");
+    bot.user.setActivity("swagy")
+});
